6eeba3363262e7d68e87515d4649c8f64a5983b4
PavanVishnuSai <pavanvishnusai@gmail.com>
2025-05-29 23:44:48 -0400

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true     
+})
+
+/* -- HOME -- */
+sr.reveal('.section__pic-container',{delay: 300})
